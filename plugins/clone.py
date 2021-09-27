2b49be4f754aaaf7da291cd949dfa627c304fe1d
rahulsinghal269 <69956800+rahulsinghal269@users.noreply.github.com>
2021-09-27 18:08:21 +0530

    Update clone.py

--- plugins/clone.py
+++ plugins/clone.py
@@ -39,9 +39,9 @@ async def clone_medias(client: Bot, message: Message):
     fn_caption = bool(query.file_caption)
     #
     if bool(clone_delay):
-        delay = 5
+        delay = 2
     else:
-        delay = 0.25
+        delay = 1
     #
     if start_id > end_id:
         start_id = start_id ^ end_id
